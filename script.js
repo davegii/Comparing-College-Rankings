bc6283f5ba0b2750226b1b399d7b5b0b3b16bd15
davegii <ji60612@gmail.com>
2025-07-26 19:08:27 -0500

    Simple bar chart to render

--- script.js
+++ script.js
@@ -37,3 +37,80 @@ Promise.all([
 
   renderScene(scene); // this will draw bar chart
 });
+
+// Scene rendering controller
+function renderScene(index) {
+    svg.selectAll("*").remove();
+    const keys = ["Times", "Shanghai", "CWUR"];
+    const key = keys[index];
+    const colorMap = {
+      "Times": "#1f77b4",
+      "Shanghai": "#1f77b4",
+      "CWUR": "#1f77b4"
+    };
+    const sceneTitles = {
+      "Times": "Scene 1: Times Higher Education Ranking (2014)",
+      "Shanghai": "Scene 2: Shanghai Ranking (2014)",
+      "CWUR": "Scene 3: CWUR Ranking (2014)"
+    };
+  
+    description.text(sceneTitles[key]);
+  
+    drawBarChart({
+      data: datasets[key],
+      color: colorMap[key]
+    });
+  }
+  
+// trigger
+d3.select("#next").on("click", () => {
+  if (scene < 2) scene++;
+  renderScene(scene);
+});
+d3.select("#prev").on("click", () => {
+  if (scene > 0) scene--;
+  renderScene(scene);
+});
+
+
+
+//functions
+
+function drawBarChart({ data, color }) {
+  const x = d3.scaleBand()
+    .domain(data.map(d => d.name))
+    .range([margin, width - margin])
+    .padding(0.1);
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.score)])
+    .nice()
+    .range([height - margin, margin]);
+
+  // bbars
+  svg.selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", d => x(d.name))
+    .attr("y", d => y(d.score))
+    .attr("width", x.bandwidth())
+    .attr("height", d => height - margin - y(d.score))
+    .attr("fill", color)
+    .append("title")
+    .text(d => `${d.name}\nScore: ${d.score}`);
+
+  // X axis
+  svg.append("g")
+    .attr("transform", `translate(0, ${height - margin})`)
+    .call(d3.axisBottom(x).tickFormat(d => d.split(" ")[0]))
+    .selectAll("text")
+    .attr("transform", "rotate(-45)")
+    .style("text-anchor", "end");
+
+  // Y-axis
+  svg.append("g")
+    .attr("transform", `translate(${margin}, 0)`)
+    .call(d3.axisLeft(y));
+}
+
