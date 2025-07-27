46895a86ca0c7a8c222c4f9a3052829cebe78e2c
davegii <ji60612@gmail.com>
2025-07-26 19:23:02 -0500

    Add country-color mapping

--- script.js
+++ script.js
@@ -6,22 +6,40 @@ const svg = d3.select("#viz").append("svg")
 const description = d3.select("#description");
 
 let datasets = {};
-//parse the data
+let schoolCountryMap = {};
+let countryColorMap = {};
 
+// Colors for countries
+const colorPalette = [
+  "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
+  "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
+  "#a6cee3", "#fb9a99", "#fdbf6f", "#cab2d6", "#ff9896",
+  "#fdb462", "#b2df8a", "#fb8072", "#80b1d3", "#fdb462"
+];
+
+//parse the data
 function parseData(data, year) {
     return data.filter(d => d.year === year)
+      .filter(d => +d.world_rank <= 100)  // Limit to top 100
+      .sort((a, b) => +a.world_rank - +b.world_rank)  // Sort by rank
       .map(d => ({
         name: d.university_name,
         score: +d.total_score,
+        country: schoolCountryMap[d.university_name] || "Unknown",
+        rank: +d.world_rank
       }));
   }
   
 
   function parseCWURData(data, year) {
     return data.filter(d => d.year === year)
+      .filter(d => +d.world_rank <= 100)  // Limit to top 100
+      .sort((a, b) => +a.world_rank - +b.world_rank)  // Sort by rank
       .map(d => ({
         name: d.institution,
-        score: +d.score
+        score: +d.score,
+        country: schoolCountryMap[d.institution] || "Unknown",
+        rank: +d.world_rank
       }));
   }
   
@@ -29,8 +47,20 @@ function parseData(data, year) {
 Promise.all([
   d3.csv("data/timesData.csv"),
   d3.csv("data/shanghaiData.csv"),
-  d3.csv("data/cwurData.csv")
-]).then(([timesData, shanghaiData, cwurData]) => {
+  d3.csv("data/cwurData.csv"),
+  d3.csv("data/school_and_country_table.csv")
+]).then(([timesData, shanghaiData, cwurData, schoolCountryData]) => {
+  //  school to country mapping
+  schoolCountryData.forEach(d => {
+    schoolCountryMap[d.school_name] = d.country;
+  });
+  
+  // country to color mapping
+  const countries = [...new Set(schoolCountryData.map(d => d.country))];
+  countries.forEach((country, index) => {
+    countryColorMap[country] = colorPalette[index % colorPalette.length];
+  });
+  
   datasets["Times"] = parseData(timesData, "2014");
   datasets["Shanghai"] = parseData(shanghaiData, "2014");
   datasets["CWUR"] = parseCWURData(cwurData, "2014");
@@ -56,10 +86,9 @@ function renderScene(index) {
   
     description.text(sceneTitles[key]);
   
-    drawBarChart({
-      data: datasets[key],
-      color: colorMap[key]
-    });
+      drawBarChart({
+    data: datasets[key]
+  });
   }
   
 // trigger
@@ -76,7 +105,7 @@ d3.select("#prev").on("click", () => {
 
 //functions
 
-function drawBarChart({ data, color }) {
+function drawBarChart({ data }) {
   const x = d3.scaleBand()
     .domain(data.map(d => d.name))
     .range([margin, width - margin])
@@ -96,9 +125,9 @@ function drawBarChart({ data, color }) {
     .attr("y", d => y(d.score))
     .attr("width", x.bandwidth())
     .attr("height", d => height - margin - y(d.score))
-    .attr("fill", color)
+    .attr("fill", d => countryColorMap[d.country] || "#cccccc")
     .append("title")
-    .text(d => `${d.name}\nScore: ${d.score}`);
+    .text(d => `${d.name}\nRank: #${d.rank}\nCountry: ${d.country}\nScore: ${d.score}`);
 
   // X axis
   svg.append("g")
@@ -112,5 +141,7 @@ function drawBarChart({ data, color }) {
   svg.append("g")
     .attr("transform", `translate(${margin}, 0)`)
     .call(d3.axisLeft(y));
+    
+
 }
 
